4821a8610f78c827dc5e466e0187a7bf3cccff10
lexer3 <lpreciado617@gmail.com>
2021-06-20 23:45:24 -0500

    Add files via upload

---
+++ ActivadadB2.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int c=0;
+float matriz1[10]={2.3,4.5,6.5,0.5,6.1,5.5,1.2,2.4,6.5,9.1};
+float matriz2[10]={2.3,4.5,6.5,0.5,6.1,5.5,1.2,2.4,6.5,9.1};
+float suma[10],resta[10],producto[10],division[10];
+
+
+
+
+return 0;
+
+}
