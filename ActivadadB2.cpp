04b82876f8e627bf2343e28651d5d79e140b87e7
lexer3 <lpreciado617@gmail.com>
2021-06-22 17:38:10 -0700

    Update ActivadadB2.cpp

--- ActivadadB2.cpp
+++ ActivadadB2.cpp
@@ -12,7 +12,7 @@ float matriz2[10]={2.3,4.5,6.5,0.5,6.1,5.5,1.2,2.4,6.5,9.1};
 float suma[10],resta[10],producto[10],division[10];
 cout<<"     PROGRAMA DE OPERACIONES MATEMATICAS: "<<endl;
 cout<<"SE MUESTRAN LAS DOS MATRICES: "<<endl;	
-	
+out<<"========================================================: "<<endl;	
 //Parte de Lexer Preciado En Esta Parte Se Muestran Por Pantalla las MATRICES 1 Y 2//
 do{
   cout<<matriz1[c];
