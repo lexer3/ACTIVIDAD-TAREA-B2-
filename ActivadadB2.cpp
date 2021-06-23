9b9c5fe69014b3571c76f46b0b21b7a3c72b5f3b
lexer3 <lpreciado617@gmail.com>
2021-06-22 17:39:39 -0700

    Update ActivadadB2.cpp

--- ActivadadB2.cpp
+++ ActivadadB2.cpp
@@ -13,34 +13,25 @@ float suma[10],resta[10],producto[10],division[10];
 cout<<"     PROGRAMA DE OPERACIONES MATEMATICAS: "<<endl;
 cout<<"SE MUESTRAN LAS DOS MATRICES: "<<endl;	
 cout<<"========================================================: "<<endl;	
-//Parte de Lexer Preciado En Esta Parte Se Muestran Por Pantalla las MATRICES 1 Y 2//
-do{
-  cout<<matriz1[c];
-  c=c+1;
-  if(c<10){
-    cout<<" - ";
-  }
+//LEXER PRECIADO TELLO MUESTRA DE MATRICES//
+  do {
+	cout<<matriz1[c];
+	c=c+1;
+	if(c<10){
+		cout<<"    ";
+	}
 }while(c<10);
-  cout<<endl;
-  c=0;
-  do{
-    cout<<matriz[c];
-    c=c+1;
-    if(c<10){
-      cout<<" -  ";
-    }
-  }while(c<10);
-  cout<<endl;
-  
-  //Lexer Preciado en esta parte mostraremos la suma de las matrices 1 y 2//
-  cout<<"\n";
-  cout<<"muestra por pantalla la suma de las matrices "<<endl;
-  cout<<"                                             "<<endl;
-  for(int c=0;c<10;c++){
-	       cout<<matriz1[c]+ matriz2[c];
-	       cout<<" - ";
-}
 cout<<endl;
+c=0;
+do {
+	cout<<matriz2[c];
+	c=c+1;
+	if(c<10){
+		cout<<"    ";
+	}
+}while(c<10);
+cout<<endl;
+c=0;
 
 return 0;
 
