aa8284705da0f4cc8cb3dbfbc9015ff7847cbfc1
lexer3 <lpreciado617@gmail.com>
2021-07-02 19:04:00 -0700

    Update ActivadadB2.cpp

--- ActivadadB2.cpp
+++ ActivadadB2.cpp
@@ -1,5 +1,5 @@
-//=============================================================================
 
+//=============================================================================
 
 
 #include<iostream>
@@ -11,12 +11,9 @@ float matriz1[10]={2.3,4.5,6.5,0.5,6.1,5.5,1.2,2.4,6.5,9.1};
 float matriz2[10]={2.3,4.5,6.5,0.5,6.1,5.5,1.2,2.4,6.5,9.1};
 float suma[10],resta[10],producto[10],division[10];
 cout<<"     PROGRAMA DE OPERACIONES MATEMATICAS: "<<endl;
-cout<<"SE MUESTRAN LAS DOS MATRICES: "<<endl;	
-cout<<"========================================================: "<<endl;	
-	
-______________________________________________
+cout<<"SE MUESTRAN LAS DOS MATRICES: "<<endl;
+cout<<"========================================================: "<<endl;
 //LEXER PRECIADO TELLO MUESTRA DE MATRICES//
-______________________________________________
   do {
 	cout<<matriz1[c];
 	c=c+1;
@@ -35,78 +32,66 @@ do {
 }while(c<10);
 cout<<endl;
 c=0;
-	
-	
-________________________________________________
-//WENDY ROBINZON RODRIGUEZ SUMA DE MATRICES//
-________________________________________________
-cout<<"=======================================:"<<endl;
-cout<<"EL RESULTADO DE LA SUMA DE MATRICES ES:"<<endl;
-do{
-suma[c]=matriz1[c]+matriz2[c];
-cout<<suma[c];
 
-         c=c+1;
-         cout<<" ";
-	
-}while(c<10);
-c=0;
-cout<<endl;
-	
-
-_______________________________________________
-//WENDY ROBINZON RODRIGUEZ RESTA DE MATRICES//
-_______________________________________________
-cout<<"EL RESULTADO DE LA RESTA DE MATRICES ES:"<<endl;
+//WENDY ROBINSON SUMA DE MATRICES 
+cout<<"========================================================: "<<endl;
+cout<<"EL RESULTADO DE LA SUMA DE MATRICES QUEDA ASI: "<<endl;
 do{
-resta[c]=matriz1[c]-matriz2[c];
-cout<<resta[c];
-
-         c=c+1;
-         cout<<" ";
+	suma[c]=matriz1[c]+matriz2[c];
+	cout<<suma[c];
 	
-}while(c<10);
-c=0;
-cout<<endl;
-cout<<"=======================================:"endl;
-	
-	
-_____________________________________________________
-//ALISSON LUCAS ANGULO MULTIPLICACION DE MATRICES//
-_____________________________________________________
-cout<<"EL RESULTADO DE LA MULTIPLICACION DE LAS MATRICES ES:"<<endl;
-do{
-producto[c]=matriz1[c]*matriz2[c];
-cout<<producto[c];
-	
-         c=c+1;
-         cout<<"   ";
+	c=c+1;
+	cout<<"    ";
 
 }while(c<10);
 c=0;
 cout<<endl;
-cout<<"======================================:"<<endl;
+
+//WENDY ROBINZON MUESTRA DE RESTA DE MATRICES//
+cout<<"========================================================: "<<endl;	
+cout<<"EL RESULTADO DE LA RESTA DE MATRICES QUEDA ASI: "<<endl;
+do{
+	resta[c]=matriz1[c]-matriz2[c];
+	cout<<resta[c];
 	
-________________________________________________
-//ALISSON LUCAS ANGULO DIVISION DE MATRICES//
-________________________________________________
-cout<<"EL RESULTADO DE LA DIVISIÓN DE MATRICES ES:"<<endl;
-for(c=0; c<10; c++){
-division[c]=matriz1[c]/matriz2[c];
-}
+	c=c+1;
+	cout<<"    ";
+
+}while(c<10);
+c=0;
+cout<<endl; 
+cout<<"========================================================: "<<endl;
+//LUCAS ANGULO ALISSON MULTIPLICACION DE MATRICES//
 	
-for(c=0; c<10; c++){
-cout<<division[c]<<"  ";
+cout<<"EL RESULTADO DE LA MULTIPLICACION DE LAS MATRICES QUEDA ASI: "<<endl;
+do{
+	producto[c]=matriz1[c]*matriz2[c];
+	cout<<producto[c];
+	
+	c=c+1;
+	cout<<"    ";
+
+}while(c<10);
+c=0;
 cout<<endl;
-}
+cout<<"========================================================: "<<endl;
+//LUCAS ANGULO ALISSON DIVISION DE MATRICES//
+
+cout<<"EL RESULTADO DE LA DIVISION DE MATRICES QUEDA ASI: "<<endl;
+
+  for (c=0;c<10;c++){
+	division[c]=matriz1[c]/matriz2[c];
+    }
+  for (c=0;c<10;c++){
+	cout<<division[c]<<"    ";
+	cout<<endl; 
+	} 
 	
-
-cout<<" "<<endl;
-cout<<"INTEGRANTES"<<endl;
-cout<<"WENDY ROBINZON RODRIGUEZ"<<endl;
-cout<<"ALISSON LUCAS ANGULO"<<endl;
-cout<<"LEXER PRECIADO TELLO"<<endl;
-
+	
+cout<<" "<<endl;	
+cout<<"INTEGRANTES "<<endl;
+cout<<"WENDY ROBINZON "<<endl;
+cout<<"LUCAS ANGULO ALISSON "<<endl;
+cout<<"LEXER PRECIADO TELLO "<<endl;
 return 0;
-
 }
